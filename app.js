a2a3d8eca5afb94a9adfd8ea6295b061bc4dc4d5
TEST USER FOR COBOL PROGRAM <33194248+testuserforcobol@users.noreply.github.com>
2017-11-12 20:43:15 +0900

    Update app.js

--- app.js
+++ app.js
@@ -39,9 +39,9 @@ app.get('/', function (req, res) {
     res.send('Hello, Facebook Messenger Bot.');
 });
 
-app.get('/webhook', function (req, res) {
-    if (req.query['hub.verify_token'] === token) {
-    res.send(req.query['hub.challenge']);
+app.get('/webhook', (req, res) => {
+    if (req.query['hub.verify_token'] === my_token) {
+      res.send(req.query['hub.challenge']);
     }
   res.send('Error, wrong validation token');
 });
